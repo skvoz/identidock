b415396c84ef425e6315c9c54efbd9430426c12c
skvoz <skvoz.ne@gmail.com>
2019-12-17 02:58:41 +0200

    first commit

---
+++ app/tests/FooTest.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace Tests;
+
+
+use PHPUnit\Framework\TestCase;
+
+class FooTest extends TestCase
+{
+    public function testFoo()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ howto.md
@@ -0,0 +1,3 @@
+##HOWTO: install jenkins, deploy with jenkins
+
+Description : this is test project for test jenkins @see https://github.com/skvoz/lessons-docker lesson6
